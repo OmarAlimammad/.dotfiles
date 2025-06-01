6a50b6d9eeb52654964a331d43410f2a366b7238
Omar <alimammadzadeomar@gmail.com>
2025-06-01 20:28:45 +0400

    .

--- nvim/lua/plugins/miscellaneous.lua
+++ nvim/lua/plugins/miscellaneous.lua
@@ -30,5 +30,20 @@ return {
   },
   {
     'stevearc/dressing.nvim',
+  },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup({
+        toggler = {
+          line = '<leader>/',
+          block = '<leader>\\',
+        },
+        opleader = {
+          line = '<leader>/',
+          block = '<leader>\\',
+        },
+      })
+    end
   }
 }
