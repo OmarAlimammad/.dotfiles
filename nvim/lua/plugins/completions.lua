2926b3a4e9a60903782d909969cfd1ad15138d7f
Omar <alimammadzadeomar@gmail.com>
2025-06-11 15:04:17 +0400

    .

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,59 +1,57 @@
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
-      'L3MON4D3/LuaSnip',
-      'windwp/nvim-autopairs',
-    },
-    config = function()
-      require('nvim-autopairs').setup({})
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
+    'L3MON4D3/LuaSnip',
+    'windwp/nvim-autopairs',
+  },
+  config = function()
+    require('nvim-autopairs').setup({})
 
-      local luasnip = require('luasnip')
-      local snippet = luasnip.snippet
-      local text = luasnip.text_node
+    local luasnip = require('luasnip')
+    local snippet = luasnip.snippet
+    local text = luasnip.text_node
 
-      luasnip.add_snippets('cpp', {
-        snippet('cd', {
-          text({
-            '#include <bits/stdc++.h>',
-            'using namespace std;',
-            '',
-            'signed main() {',
-            '\tios::sync_with_stdio(0);',
-            '\tcin.tie(nullptr);',
-            '\t',
-            '}'
-          }),
+    luasnip.add_snippets('cpp', {
+      snippet('cd', {
+        text({
+          '#include <bits/stdc++.h>',
+          'using namespace std;',
+          '',
+          'signed main() {',
+          '\tios::sync_with_stdio(0);',
+          '\tcin.tie(nullptr);',
+          '\t',
+          '}'
         }),
-      })
+      }),
+    })
 
-      local cmp = require('cmp')
-      cmp.setup({
-        window = {
-          completion = cmp.config.window.bordered({
-            border = 'rounded',
-            scrollbar = false,
-          }),
-          documentation = cmp.config.window.bordered({
-            border = 'rounded',
-            scrollbar = false,
-          }),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<Down>'] = cmp.mapping.select_next_item(),
-          ['<Up>'] = cmp.mapping.select_prev_item(),
-          ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-          ['<CR>'] = cmp.mapping.abort(),
+    local cmp = require('cmp')
+    cmp.setup({
+      window = {
+        completion = cmp.config.window.bordered({
+          border = 'rounded',
+          scrollbar = false,
         }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
+        documentation = cmp.config.window.bordered({
+          border = 'rounded',
+          scrollbar = false,
         }),
-      })
-    end
-  }
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<Down>'] = cmp.mapping.select_next_item(),
+        ['<Up>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.abort(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }),
+    })
+  end
 }
 
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,72 +1,70 @@
 return {
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'williamboman/mason.nvim',
-      'ray-x/lsp_signature.nvim',
-    },
-    config = function()
-      require('mason').setup({
-        ui = {
-          border = 'rounded',
-        },
-      })
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'ray-x/lsp_signature.nvim',
+  },
+  config = function()
+    require('mason').setup({
+      ui = {
+        border = 'rounded',
+      },
+    })
 
-      require('lsp_signature').setup({
-        bind = true,
-        handler_opts = {
-          border = 'rounded',
-        },
-        floating_window = true,
-        hint_enable = false,
-      })
+    require('lsp_signature').setup({
+      bind = true,
+      handler_opts = {
+        border = 'rounded',
+      },
+      floating_window = true,
+      hint_enable = false,
+    })
 
-      local lspconfig = require('lspconfig')
+    local lspconfig = require('lspconfig')
 
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.clangd.setup({
-        cmd = { 'clangd', '--header-insertion=never', '--function-arg-placeholders=false' }
-      })
+    lspconfig.lua_ls.setup({})
+    lspconfig.pyright.setup({})
+    lspconfig.clangd.setup({
+      cmd = { 'clangd', '--header-insertion=never', '--function-arg-placeholders=false' }
+    })
 
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = '',
-          format = function(diagnostic)
-            local icons = {
-              [vim.diagnostic.severity.ERROR] = ' ',
-              [vim.diagnostic.severity.WARN]  = ' ',
-              [vim.diagnostic.severity.INFO]  = ' ',
-              [vim.diagnostic.severity.HINT]  = ' ',
-            }
-            return icons[diagnostic.severity] .. diagnostic.message
-          end,
+    vim.diagnostic.config({
+      virtual_text = {
+        prefix = '',
+        format = function(diagnostic)
+          local icons = {
+            [vim.diagnostic.severity.ERROR] = ' ',
+            [vim.diagnostic.severity.WARN]  = ' ',
+            [vim.diagnostic.severity.INFO]  = ' ',
+            [vim.diagnostic.severity.HINT]  = ' ',
+          }
+          return icons[diagnostic.severity] .. diagnostic.message
+        end,
+      },
+      float = {
+        header = '',
+        prefix = '',
+        border = 'rounded',
+        style = 'minimal',
+      },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN]  = '',
+          [vim.diagnostic.severity.INFO]  = '',
+          [vim.diagnostic.severity.HINT]  = '',
         },
-        float = {
-          header = '',
-          prefix = '',
-          border = 'rounded',
-          style = 'minimal',
-        },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '',
-            [vim.diagnostic.severity.WARN]  = '',
-            [vim.diagnostic.severity.INFO]  = '',
-            [vim.diagnostic.severity.HINT]  = '',
-          },
-        },
-        severity_sort = true,
-        underline = true,
-        update_in_insert = false,
-      })
+      },
+      severity_sort = true,
+      underline = true,
+      update_in_insert = false,
+    })
 
-      local orig_util = vim.lsp.util.open_floating_preview
-      vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
-        opts = opts or {}
-        opts.border = opts.border or 'rounded'
-        return orig_util(contents, syntax, opts, ...)
-      end
+    local orig_util = vim.lsp.util.open_floating_preview
+    vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or 'rounded'
+      return orig_util(contents, syntax, opts, ...)
     end
-  }
+  end
 }
